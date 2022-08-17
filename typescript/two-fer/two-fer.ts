a136a0f0028f2989bd0a6f3bf9d8df385980680d
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2022-08-17 09:19:51 -0300

    Delete typescript/two-fer directory

--- typescript/two-fer/two-fer.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This stub is provided to make it straightforward to get started.
- */
-
-export function twoFer(name?: string): string {
-  return `One for ${name || "you"}, one for me.`
-}
-
-export default twoFer;
