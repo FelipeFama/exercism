a744d1602a5ec22261f8103b1bc0031612e0a9b3
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-02-25 20:48:59 -0300

    secret handshake javascript

---
+++ javascript/secret-handshake/secret-handshake.js
@@ -0,0 +1,31 @@
+//
+// This is only a SKELETON file for the 'Secret Handshake' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const commands = (number) => {
+  let binary = number.toString(2)
+  let result = []
+  
+  while(binary.length < 5) {
+    binary = "0" + binary;
+  }
+  
+  if(binary[4] == "1") {
+    result.push("wink");
+  }
+  if(binary[3] == "1") {
+    result.push("double blink");
+  }
+  if(binary[2] == "1") {
+    result.push("close your eyes");
+  }
+  if(binary[1] == "1") {
+    result.push("jump");
+  }
+  if(binary[0] == "1") {
+    result.reverse();
+  }
+  
+  return result;
+};
