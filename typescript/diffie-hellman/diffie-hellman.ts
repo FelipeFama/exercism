e97c3efe74ef4ed6c5063b6f50974eae7c81b080
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-03-01 21:31:05 -0300

    diffie hellman typescript

---
+++ typescript/diffie-hellman/diffie-hellman.ts
@@ -0,0 +1,34 @@
+export class DiffieHellman {
+  private p: number;
+  private g: number;
+
+  static isPrime(num: number): boolean {
+    for(let i = 2; i < Math.floor(num / 2); i++) {
+      if(num % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  constructor(p: number, g: number) {
+    if(p < 1 || p > 9998 || g < 1 || g > 9998) {
+      throw new Error('')
+    } else if(!DiffieHellman.isPrime(p) || !DiffieHellman.isPrime(g)) {
+      throw new Error('')
+    }
+
+    this.p = p;
+    this.g = g;
+  }
+
+  public getPublicKey(privateKey: number): number {
+    if(privateKey > 1 && privateKey < this.p)
+      return (this.g ** privateKey) % this.p
+    throw new Error('')
+  }
+
+  public getSecret(theirPublicKey: number, myPrivateKey: number): number {
+    return (theirPublicKey ** myPrivateKey) % this.p
+  }
+}
