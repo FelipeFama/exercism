62291cf1fb7130af4020528cfc6a1705d6a775d9
Felipe Fama <lipehfama.dev@gmail.com>
2025-04-17 21:48:52 -0300

    pangram java

---
+++ java/pangram/src/main/java/PangramChecker.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class PangramChecker {
+
+    public boolean isPangram(String input) {
+        Set<Character> letters = new HashSet<>();
+        for(char c: input.toLowerCase().toCharArray()) {
+            if(c >= 'a' && c <= 'z') {
+                letters.add(c);
+            }
+        }
+        return letters.size() == 26;
+    }
+
+}
